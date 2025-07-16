9b377992aed8f4caa25e9d396dc49576abc19259
Axel Leonel <alessoaxel@gmail.com>
2025-07-16 14:58:59 -0300

    holaa

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,36 +3,10 @@ import { Product } from '../product/product';
 
 @Component({
   selector: 'app-home',
-  imports: [Product],
+  imports: [],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-    productos =[
-      {
-        nombre:'producto1',
-        precio:10,
-        id:1
-      },
-      {
-        nombre:'producto2',
-        precio:20,
-        id:2
-      },
-      {
-        nombre:'producto3',
-        precio:30,
-        id:3
-      },
-      {
-        nombre:'producto4',
-        precio:40,
-        id:4
-      },
-      {
-        nombre:'producto5',
-        precio:50,
-        id:5
-      }
-    ]
+    
 }
--- src/app/product/product.ts
+++ src/app/product/product.ts
@@ -2,7 +2,6 @@ import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-product',
-  imports: [],
   templateUrl: './product.html',
   styleUrl: './product.css'
 })
